cc39da3b05840a5af307e572cb224293a29ab1ff
Andrea Lo Cicero <jpgflipper@gmail.com>
2023-04-19 16:08:54 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="card">
+      <div className="image">
+        <img src="src/assets/image-qr-code.png" />
+      </div>
+      <div className="text">
+        <h1>Improve your front-end skills by building projects</h1>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
